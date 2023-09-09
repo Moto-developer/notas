76d687c2d67b37a55d7b5db648249793c2d737d8
moto-developer <yecidp21@gmail.com>
2023-09-09 01:46:51 -0500

    adelanto trabajo rider

--- notas.js
+++ notas.js
@@ -2,14 +2,29 @@ const registroN = document.querySelector('.registroN');
 const cerrar = document.querySelector('#cerrar');
 const modal = document.querySelector('.modal');
 const resultados = document.getElementById('resultados');
+const resulNotas = document.querySelector('.resulNotas');
 
+const parciales = {
+    nombre:'',
+    nota1:'',
+    nota2:'',
+    nota3:'',
+    definitiva:''
+}
 
-
-
-const boton = (nota1,nota2,nota3) => {
+const boton = (nombre,nota1,nota2,nota3) => {
     let notas = [nota1, nota2, nota3];
     notDefinitiva = nueva(notas,porcentaje);
+    
+    parciales.nombre=nombre
+    parciales.nota1=nota1;
+    parciales.nota2=nota2;
+    parciales.nota3=nota3;
+    parciales.definitiva=notDefinitiva;
 
+    impNotas();
+
+    console.log(parciales);
     console.log(notas);
     console.log(notDefinitiva);
 
@@ -23,6 +38,7 @@ function porcentaje(suma){
     return mostrar;
     
 };
+
 const nueva = (notas,porcentaje) => {
     let suma = 0;
     notas.forEach(nota => ( suma += nota));
@@ -43,3 +59,11 @@ cerrar.addEventListener('click', ()=>{
 });
 
 //registrar notas
+const impNotas = ()=>{
+    let nuevoR = document.createElement('tr');
+    nuevoR.innerHTML=`<td>${parciales.nombre}</td> <td>${parciales.nota1}</td>
+    <td>${parciales.nota2}</td> <td>${parciales.nota3}</td> <td>${parciales.definitiva}</td> <td><span class="material-symbols-outlined">delete_forever</span> <span class="material-symbols-outlined">edit</span></td>`;
+    resulNotas.appendChild(nuevoR);
+
+    resultados.style='display: block'
+}
